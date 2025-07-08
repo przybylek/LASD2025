3d833656410606d457932b2ac17f7b83f7f7ce50
Adam Przybylek <adam.przybylek@gmail.com>
2025-07-08 18:09:38 +0200

    conference year fixed

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,7 +4,7 @@ import Image from 'react-bootstrap/Image'
 
 const Navigation = () => (
   <Navbar sticky='top' bg='white' className='shadow-sm' expand='xl'>
-    <Navbar.Brand href='#home'>LASD@ISD'24</Navbar.Brand>
+    <Navbar.Brand href='#home'>LASD@ISD'25</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbar-nav" />
     <Navbar.Collapse id="navbar-nav">
       <Nav className="ml-auto">
